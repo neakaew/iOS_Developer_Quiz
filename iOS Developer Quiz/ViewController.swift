d2565a111b60cec1ef7b4e394610cbdf1802b105
Udom039 <pokemoner799@gmail.com>
2019-06-26 18:01:40 +0700

    Initial Commit

---
+++ iOS Developer Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Developer Quiz
+//
+//  Created by udom on 26/6/2562 BE.
+//  Copyright © 2562 udom Neakaew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS Developer QuizTests/iOS_Developer_QuizTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Developer_QuizTests.swift
+//  iOS Developer QuizTests
+//
+//  Created by udom on 26/6/2562 BE.
+//  Copyright © 2562 udom Neakaew. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Developer_Quiz
+
+class iOS_Developer_QuizTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
